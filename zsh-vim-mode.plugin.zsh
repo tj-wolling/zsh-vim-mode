62357bfdcc93faafd2dd3b18b41fd605d88bb106
Tim Smith <tim.dolores@gmail.com>
2015-09-09 17:19:58 -0600

    Merge in keybindings from pjg/dotfiles/.zshrc, etc.
    General guideline: If ^X (for some X) works in emacs mode, and isn't
    already doing something more important in viins mode, it should be made
    to behave like emacs mode. Mostly true for vicmd, too. This change gets
    a bit closer to that.

--- zsh-vim-mode.plugin.zsh
+++ zsh-vim-mode.plugin.zsh
@@ -1,84 +1,135 @@
-bindkey -v
+# Zsh's history-beginning-search-backward is very close to Vim's C-x C-l
+history-beginning-search-backward-then-append() {
+  zle history-beginning-search-backward
+  zle vi-add-eol
+}
+zle -N history-beginning-search-backward-then-append
 
-# http://dougblack.io/words/zsh-vi-mode.html
+bindkey -v
 export KEYTIMEOUT=1
 
-bindkey -M viins '^k' vi-cmd-mode # <C-k> for going to command mode
-bindkey -M vicmd ' ' execute-named-cmd # Space for command line mode
+# Edit command line
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey -M viins '^x^e'  edit-command-line
 
-# Home key variants
-bindkey '\e[1~' vi-beginning-of-line
-bindkey '\eOH' vi-beginning-of-line
+bindkey -M viins '^a'    beginning-of-line
+bindkey -M viins '^e'    end-of-line
+bindkey -M viins '^b'    backward-char
+bindkey -M viins '^d'    delete-char-or-list
+bindkey -M viins '^f'    forward-char
+bindkey -M viins '^k'    kill-line
+bindkey -M viins '^r'    history-incremental-pattern-search-backward
+bindkey -M viins '^s'    history-incremental-pattern-search-forward
+bindkey -M viins '^o'    history-beginning-search-backward
+bindkey -M viins '^p'    up-line-or-history
+bindkey -M viins '^n'    down-line-or-history
+bindkey -M viins '^y'    yank
+bindkey -M viins '^w'    backward-kill-word
+bindkey -M viins '^u'    backward-kill-line
+bindkey -M viins '^h'    backward-delete-char
+bindkey -M viins '^?'    backward-delete-char
+bindkey -M viins '^_'    undo
+bindkey -M viins '^x^l'  history-beginning-search-backward-then-append
+bindkey -M viins '^x^r'  redisplay
+bindkey -M viins '\eb'   backward-word
+bindkey -M viins '\ed'   kill-word
+bindkey -M viins '\ef'   forward-word
+bindkey -M viins '\eh'   run-help
+bindkey -M viins '\e.'   insert-last-word
+# Alt + arrows
+bindkey -M viins '[D'    backward-word
+bindkey -M viins '[C'    forward-word
+bindkey -M viins '\e[1;3D' backward-word
+bindkey -M viins '\e[1;3C' forward-word
+# Ctrl + arrows
+bindkey -M viins '\eOD'  backward-word
+bindkey -M viins '\eOC'  forward-word
+bindkey -M viins '\e[1;5D' backward-word
+bindkey -M viins '\e[1;5C' forward-word
+# Home / End
+bindkey -M viins '\eOH'  beginning-of-line
+bindkey -M viins '\eOF'  end-of-line
+bindkey -M viins '\e[1~' beginning-of-line
+bindkey -M viins '\e[4~' end-of-line
+# Insert / Delete
+bindkey -M viins '\e[2~' overwrite-mode
+bindkey -M viins '\e[3~' delete-char
+# Page up / Page down
+bindkey -M viins '\e[5~' history-beginning-search-backward
+bindkey -M viins '\e[6~' history-beginning-search-forward
+# Shift + Tab
+bindkey -M viins '\e[Z'  reverse-menu-complete
 
-# End key variants
-bindkey '\e[4~' vi-end-of-line
-bindkey '\eOF' vi-end-of-line
-
-bindkey -M viins '^o' vi-backward-kill-word
-
-bindkey -M vicmd 'yy' vi-yank-whole-line
-bindkey -M vicmd 'Y' vi-yank-eol
-
-bindkey -M vicmd 'y.' vi-yank-whole-line
-bindkey -M vicmd 'c.' vi-change-whole-line
-bindkey -M vicmd 'd.' kill-whole-line
-
-bindkey -M vicmd 'u' undo
-bindkey -M vicmd 'U' redo
-
-bindkey -M vicmd 'H' run-help
-bindkey -M viins '\eh' run-help
+# TODO Use zsh 5.0.8 text objects & visual mode
+#bindkey -M vicmd 'ca'    change-around
+#bindkey -M vicmd 'ci'    change-in
+#bindkey -M vicmd 'da'    delete-around
+#bindkey -M vicmd 'di'    delete-in
+bindkey -M vicmd 'ga'    what-cursor-position
+bindkey -M vicmd 'gg'    beginning-of-history
+bindkey -M vicmd 'G '    end-of-history
+bindkey -M vicmd '^a'    beginning-of-line
+bindkey -M vicmd '^b'    backward-char
+bindkey -M vicmd '^e'    end-of-line
+bindkey -M vicmd '^f'    forward-char
+bindkey -M vicmd '^i'    history-substring-search-down
+bindkey -M vicmd '^k'    kill-line
+bindkey -M vicmd '^r'    history-incremental-pattern-search-backward
+bindkey -M vicmd '^s'    history-incremental-pattern-search-forward
+bindkey -M vicmd '^o'    history-beginning-search-backward
+bindkey -M vicmd '^p'    up-line-or-history
+bindkey -M vicmd '^n'    down-line-or-history
+bindkey -M vicmd '^y'    yank
+bindkey -M vicmd '^w'    backward-kill-word
+bindkey -M vicmd '^u'    backward-kill-line
+bindkey -M vicmd '^_'    undo
+bindkey -M vicmd '/'     vi-history-search-forward
+bindkey -M vicmd '?'     vi-history-search-backward
+bindkey -M vicmd ':'     execute-named-cmd
+bindkey -M vicmd '\eb'   backward-word
+bindkey -M vicmd '\ed'   kill-word
+bindkey -M vicmd '\ef'   forward-word
+bindkey -M vicmd '\e.'   insert-last-word
+# Page up / Page down
+bindkey -M vicmd '\e[5~' history-beginning-search-backward
+bindkey -M vicmd '\e[6~' history-beginning-search-forward
+bindkey -M vicmd 'H'     run-help
+bindkey -M vicmd 'u'     undo
+bindkey -M vicmd 'U'     redo
+bindkey -M vicmd 'Y'     vi-yank-eol
+bindkey -M vicmd '\-'    vi-repeat-find
+bindkey -M vicmd '_'     vi-rev-repeat-find
 
 if [[ -n $HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND ]]; then
-    bindkey -M vicmd 'k' history-substring-search-up
-    bindkey -M vicmd 'j' history-substring-search-down
+    bindkey -M viins '^o' history-substring-search-up
+    bindkey -M viins '\e[5~' history-substring-search-up
+    bindkey -M viins '\e[6~' history-substring-search-down
 
-    bindkey '^p' history-substring-search-up
-    bindkey '^n' history-substring-search-down
-else
-    bindkey -M vicmd 'k' up-history
-    bindkey -M vicmd 'j' down-history
-
-    bindkey '^p' up-history
-    bindkey '^n' down-history
+    bindkey -M vicmd '^o' history-substring-search-up
+    bindkey -M vicmd '^i' history-substring-search-down
+    bindkey -M vicmd '\e[5~' history-substring-search-up
+    bindkey -M vicmd '\e[6~' history-substring-search-down
 fi
 
-bindkey -M vicmd '\-' vi-repeat-find
-bindkey -M vicmd '_' vi-rev-repeat-find
-bindkey -M viins '^r' vi-rev-repeat-find
-
-bindkey -M viins '\e.' insert-last-word
-bindkey -M vicmd '\e.' insert-last-word
-
-bindkey -M viins '^a' beginning-of-line
-bindkey -M viins '^e' end-of-line
-
-# backspace and ^h working even after returning from command mode
-bindkey -M viins '^?' backward-delete-char
-bindkey -M viins '^h' backward-delete-char
-
 # If mode indicator wasn't setup by theme, define default
-if [[ "$N_MODE" == "" ]]; then
+if [[ -z $N_MODE ]]; then
   N_MODE="%{$fg[red]%}[N]%{$reset_color%}"
 fi
 
-if [[ "$I_MODE" == "" ]]; then
+if [[ -z $I_MODE ]]; then
   I_MODE="%{$fg[white]%}[I]%{$reset_color%}"
 fi
 
 function zle-line-init zle-keymap-select {
   case $KEYMAP in
-    vicmd)
-      RPROMPT=$N_MODE
-      ;;
-    main|viins)
-      RPROMPT=$I_MODE
-      ;;
+    vicmd) RPROMPT=$N_MODE ;;
+    main|viins) RPROMPT=$I_MODE ;;
   esac
   zle reset-prompt
 }
 
-# define right prompt, if it wasn't defined by a theme
+# Define right prompt, if it wasn't defined by a theme
 if [[ -z $RPS1 && -z $RPROMPT ]]; then
     zle -N zle-line-init
     zle -N zle-keymap-select
