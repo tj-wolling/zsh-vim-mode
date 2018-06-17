0201bd70c3c4f87d4a40311b05fdd80d758bf579
Tim Smith <tim.dolores@gmail.com>
2018-06-16 23:32:06 -0600

    Use same escape format as bindkey, no behavior changes

--- zsh-vim-mode.plugin.zsh
+++ zsh-vim-mode.plugin.zsh
@@ -4,63 +4,63 @@ bindkey -v
 export KEYTIMEOUT=5
 
 # viins - Basic Emacs-like bindings {{{1
-bindkey -M viins '^a'    beginning-of-line
-bindkey -M viins '^e'    end-of-line
-bindkey -M viins '^b'    backward-char
-bindkey -M viins '^d'    delete-char-or-list
-bindkey -M viins '^f'    forward-char
-bindkey -M viins '^k'    kill-line
-bindkey -M viins '^r'    history-incremental-pattern-search-backward
-bindkey -M viins '^s'    history-incremental-pattern-search-forward
-bindkey -M viins '^y'    yank
-bindkey -M viins '^w'    backward-kill-word
-bindkey -M viins '^u'    backward-kill-line
-bindkey -M viins '^h'    backward-delete-char
+bindkey -M viins '^A'    beginning-of-line
+bindkey -M viins '^B'    backward-char
+bindkey -M viins '^D'    delete-char-or-list
+bindkey -M viins '^E'    end-of-line
+bindkey -M viins '^F'    forward-char
+bindkey -M viins '^H'    backward-delete-char
+bindkey -M viins '^K'    kill-line
+bindkey -M viins '^R'    history-incremental-pattern-search-backward
+bindkey -M viins '^S'    history-incremental-pattern-search-forward
+bindkey -M viins '^U'    backward-kill-line
+bindkey -M viins '^W'    backward-kill-word
+bindkey -M viins '^Y'    yank
 bindkey -M viins '^?'    backward-delete-char
 bindkey -M viins '^_'    undo
-bindkey -M viins '^x^r'  redisplay
-bindkey -M viins '\eb'   backward-word
-bindkey -M viins '\ed'   kill-word
-bindkey -M viins '\ef'   forward-word
-bindkey -M viins '\eh'   run-help
-bindkey -M viins '\e.'   insert-last-word
+bindkey -M viins '^X^R'  redisplay
+bindkey -M viins '^[b'   backward-word
+bindkey -M viins '^[d'   kill-word
+bindkey -M viins '^[f'   forward-word
+bindkey -M viins '^[h'   run-help
+bindkey -M viins '^[.'   insert-last-word
 # Alt + arrows
 bindkey -M viins '[D'    backward-word
 bindkey -M viins '[C'    forward-word
-bindkey -M viins '\e[1;3D' backward-word
-bindkey -M viins '\e[1;3C' forward-word
+bindkey -M viins '^[[1;3D' backward-word
+bindkey -M viins '^[[1;3C' forward-word
 # Ctrl + arrows
-bindkey -M viins '\eOD'  backward-word
-bindkey -M viins '\eOC'  forward-word
-bindkey -M viins '\e[1;5D' backward-word
-bindkey -M viins '\e[1;5C' forward-word
+bindkey -M viins '^[OD'  backward-word
+bindkey -M viins '^[OC'  forward-word
+bindkey -M viins '^[[1;5D' backward-word
+bindkey -M viins '^[[1;5C' forward-word
 # Home / End
-bindkey -M viins '\eOH'  beginning-of-line
-bindkey -M viins '\eOF'  end-of-line
-bindkey -M viins '\e[1~' beginning-of-line
-bindkey -M viins '\e[4~' end-of-line
+bindkey -M viins '^[OH'  beginning-of-line
+bindkey -M viins '^[OF'  end-of-line
+bindkey -M viins '^[[1~' beginning-of-line
+bindkey -M viins '^[[4~' end-of-line
 # Insert / Delete
-bindkey -M viins '\e[2~' overwrite-mode
-bindkey -M viins '\e[3~' delete-char
+bindkey -M viins '^[[2~' overwrite-mode
+bindkey -M viins '^[[3~' delete-char
 # Shift + Tab
-bindkey -M viins '\e[Z'  reverse-menu-complete
+bindkey -M viins '^[[Z'  reverse-menu-complete
 
 # vicmd - Basic Emacs-like bindings {{{1
-bindkey -M vicmd '^a'    beginning-of-line
-bindkey -M vicmd '^b'    backward-char
-bindkey -M vicmd '^e'    end-of-line
-bindkey -M vicmd '^f'    forward-char
-bindkey -M vicmd '^k'    kill-line
-bindkey -M vicmd '^r'    history-incremental-pattern-search-backward
-bindkey -M vicmd '^s'    history-incremental-pattern-search-forward
-bindkey -M vicmd '^y'    yank
-bindkey -M vicmd '^w'    backward-kill-word
-bindkey -M vicmd '^u'    backward-kill-line
+bindkey -M vicmd '^A'    beginning-of-line
+bindkey -M vicmd '^B'    backward-char
+bindkey -M vicmd '^E'    end-of-line
+bindkey -M vicmd '^F'    forward-char
+bindkey -M vicmd '^K'    kill-line
+bindkey -M vicmd '^R'    history-incremental-pattern-search-backward
+bindkey -M vicmd '^S'    history-incremental-pattern-search-forward
+bindkey -M vicmd '^U'    backward-kill-line
+bindkey -M vicmd '^W'    backward-kill-word
+bindkey -M vicmd '^Y'    yank
 bindkey -M vicmd '^_'    undo
-bindkey -M vicmd '\eb'   backward-word
-bindkey -M vicmd '\ed'   kill-word
-bindkey -M vicmd '\ef'   forward-word
-bindkey -M vicmd '\e.'   insert-last-word
+bindkey -M vicmd '^[b'   backward-word
+bindkey -M vicmd '^[d'   kill-word
+bindkey -M vicmd '^[f'   forward-word
+bindkey -M vicmd '^[.'   insert-last-word
 bindkey -M vicmd 'H'     run-help
 bindkey -M vicmd 'U'     redo
 bindkey -M vicmd 'Y'     vi-yank-eol
@@ -68,32 +68,32 @@ bindkey -M vicmd 'Y'     vi-yank-eol
 # edit-command-line {{{1
 autoload -U edit-command-line
 zle -N edit-command-line
-bindkey -M viins '^x^e'  edit-command-line
-bindkey -M vicmd '^v'    edit-command-line
+bindkey -M viins '^X^E'  edit-command-line
+bindkey -M vicmd '^V'    edit-command-line
 
 # history-substring-search {{{1
 if [[ -n $HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND ]]; then
-    bindkey -M viins '^p'    history-substring-search-up
-    bindkey -M viins '^n'    history-substring-search-down
-    bindkey -M vicmd '^p'    history-substring-search-up
-    bindkey -M vicmd '^n'    history-substring-search-down
+    bindkey -M viins '^P'    history-substring-search-up
+    bindkey -M viins '^N'    history-substring-search-down
+    bindkey -M vicmd '^P'    history-substring-search-up
+    bindkey -M vicmd '^N'    history-substring-search-down
 
     # Page up / Page down
-    bindkey -M viins '\e[5~' history-substring-search-up
-    bindkey -M viins '\e[6~' history-substring-search-down
-    bindkey -M vicmd '\e[5~' history-substring-search-up
-    bindkey -M vicmd '\e[6~' history-substring-search-down
+    bindkey -M viins '^[[5~' history-substring-search-up
+    bindkey -M viins '^[[6~' history-substring-search-down
+    bindkey -M vicmd '^[[5~' history-substring-search-up
+    bindkey -M vicmd '^[[6~' history-substring-search-down
 else
-    bindkey -M viins '^p' history-beginning-search-backward
-    bindkey -M viins '^n' history-beginning-search-forward
-    bindkey -M vicmd '^p' history-beginning-search-backward
-    bindkey -M vicmd '^n' history-beginning-search-forward
+    bindkey -M viins '^P' history-beginning-search-backward
+    bindkey -M viins '^N' history-beginning-search-forward
+    bindkey -M vicmd '^P' history-beginning-search-backward
+    bindkey -M vicmd '^N' history-beginning-search-forward
 
     # Page up / Page down
-    bindkey -M viins '\e[5~' history-beginning-search-backward
-    bindkey -M viins '\e[6~' history-beginning-search-forward
-    bindkey -M vicmd '\e[5~' history-beginning-search-backward
-    bindkey -M vicmd '\e[6~' history-beginning-search-forward
+    bindkey -M viins '^[[5~' history-beginning-search-backward
+    bindkey -M viins '^[[6~' history-beginning-search-forward
+    bindkey -M vicmd '^[[5~' history-beginning-search-backward
+    bindkey -M vicmd '^[[6~' history-beginning-search-forward
 fi
 
 
